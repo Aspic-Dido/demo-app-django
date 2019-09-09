ca6e0c378f75793337011cb8b9dd02ba9d510c96
Aspic-Dido <dieudonnemiyalu6@gmail.com>
2019-09-09 19:19:36 +0200

    Add STATIC_ROOT configuration

--- b25tech/settings.py
+++ b25tech/settings.py
@@ -120,15 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 #Extra lookup directories for collectfiles to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
     )
+
+INTERNAL_IPS = ['127.0.0.1']
 
 #Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
